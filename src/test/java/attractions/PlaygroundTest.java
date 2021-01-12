416792b9c6235887abc8ba2b084cf0c3ac20b328
Ruth Foulis <ruthlilfoulis@gmail.com>
2021-01-12 17:55:31 +0000

    test passes for playground ISecurity

---
+++ src/test/java/attractions/PlaygroundTest.java
@@ -0,0 +1,44 @@
+package attractions;
+
+import org.junit.Before;
+import org.junit.Test;
+import people.Visitor;
+
+import static org.junit.Assert.assertEquals;
+
+public class PlaygroundTest {
+    Playground playground;
+    Visitor visitor;
+
+    @Before
+    public void setUp() throws Exception {
+        playground = new Playground("Fun Zone", 7);
+        visitor = new Visitor(14, 1.2, 40.0);
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Fun Zone", playground.getName());
+    }
+
+    @Test
+    public void hasRating() {
+        assertEquals(7, playground.getRating());
+    }
+
+    @Test
+    public void hasVisitCount() {
+        assertEquals(0, playground.getVisitCount());
+    }
+
+    @Test
+    public void visitorIsAllowedToEnter(){
+        assertEquals(true, playground.isAllowedTo(visitor));
+    }
+
+    @Test
+    public void visitorIsAllowedToEnter__False(){
+        Visitor visitor2 = new Visitor(16, 1.4, 23);
+        assertEquals(false, playground.isAllowedTo(visitor2));
+    }
+}
