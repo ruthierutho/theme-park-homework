4672b30fa579595455ee1e5ea8229357b673544b
Ruth Foulis <ruthlilfoulis@gmail.com>
2021-01-12 18:24:09 +0000

    dodgems implements ITicketed MVP done

--- src/test/java/attractions/DodgemTest.java
+++ src/test/java/attractions/DodgemTest.java
@@ -2,6 +2,7 @@ package attractions;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
 import static org.junit.Assert.assertEquals;
 
@@ -28,5 +29,16 @@ public class DodgemTest {
     @Test
     public void hasVisitCount() {
         assertEquals(0, dodgems.getVisitCount());
+    }
+
+    @Test
+    public void getDefaultPrice(){
+        assertEquals(4.50, dodgems.defaultPrice(), 0.01);
+    }
+
+    @Test
+    public void chargerUnder12sHalf(){
+        Visitor visitor2 = new Visitor(10, 110, 6);
+        assertEquals(2.25, dodgems.priceFor(visitor2), 0.01);
     }
 }
